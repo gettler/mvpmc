809498d6072623aac476a3ae72ec8d20d17cc30a
Jon Gettler <gettler@mvpmc.org>
2008-04-24 22:46:47 -0500

    NMT: Clean up the web pages a bit to make it easier to start from gaya.

---
+++ dongle/filesystem/tree/nmt/etc/mvpmc.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+OUTPUT=/tmp/output.html
+
+TOP=/opt/sybhttpd/localhost.drives
+
+MOUNT=${SCRIPT_NAME%%mvpmc*}
+
+cat > $OUTPUT <<EOF
+<h1>mvpmc</h1>
+<hr>
+<h2>Select an option:</h2>
+<ul>
+<li><a href="http://localhost.drives:8883${MOUNT}mvpmc/etc/script-telnet.cgi">Start telnetd</a></li>
+<li><a href="http://localhost.drives:8883${MOUNT}mvpmc/etc/script-mvpmc.cgi">Start mvpmc</a></li>
+</ul>
+EOF
+
+LEN=`wc -c $OUTPUT`
+
+echo "Content-Length: $LEN";
+echo "Content-type: text/html";
+echo "";
+
+cat $OUTPUT
+
+exit 0
